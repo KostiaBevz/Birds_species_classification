ff8d82dd7e15469153166710ea6a3be2cc763bad
Kostiantyn <battelfield567@gmail.com>
2023-03-08 01:57:10 +0200

    small fix

---
+++ src/data_creation/get_data_kaggle.py
@@ -0,0 +1,54 @@
+import os
+import sys
+
+import pandas as pd
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+sys.path.append("./")
+# TODO: add logging, command line
+# import click
+# import logging
+import config
+
+"""
+If I want to download data from competition need to use
+    api.competition_download_cli(competition_opt=COMPETITION, path=PATH) or
+    api.competition_download_file() or
+    api.competition_download_files()
+
+"""
+if __name__ == "__main__":
+    api = KaggleApi()
+    api.authenticate()
+    path = config.DATA_DIR[:-1]
+    dataset = config.DATASET
+    file_name = config.ANNOTATION_FILE_NAME
+
+    api.dataset_download_cli(dataset=dataset, path=path, unzip=True)
+
+    # Generate annotations
+
+    if file_name not in os.listdir(path):
+        df = pd.DataFrame(
+            columns=["class_id", "class_name", "absolute_path", "dataset_type"]
+        )
+
+        directories = ["test/", "train/", "validation/"]
+
+        data = dict()
+        for directory in directories:
+            data["dataset_type"] = directory[:-1]
+            directory_path = os.path.join(path, directory)
+            for indx, class_name in enumerate(os.listdir(directory_path)):
+                data["class_id"] = indx
+                data["class_name"] = class_name
+                class_directory_path = os.path.join(
+                    directory_path, class_name + "/"
+                )
+                for img in os.listdir(class_directory_path):
+                    img_path = os.path.join(class_directory_path, img)
+                    data["absolute_path"] = img_path
+                    entry = pd.DataFrame([data])
+                    df = pd.concat([df, entry], ignore_index=True)
+
+        df.to_csv(path + file_name, index=False)
